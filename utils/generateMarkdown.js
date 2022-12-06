d1bccbcd3e67d9ce557748f110ce069d274f9d72
yerimechoi <yerimechoi@gmail.com>
2022-12-06 00:21:57 -0500

    Reduced license to three and added badge, link and license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,100 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  Apache License 2.0 
+  [![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0) 
+
+  MIT License
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+
+  ISC License
+  [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+  N/A
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  Apache License 2.0 
+  `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+
+  MIT License
+  `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+
+  ISC License
+  `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+
+  N/A
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // Apache License 2.0 
+  // [   Copyright [yyyy] [name of copyright owner]
+
+  // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+  //   http://www.apache.org/licenses/LICENSE-2.0
+
+  // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.]
+
+  // MIT License
+  // [Copyright <YEAR> <COPYRIGHT HOLDER>
+
+  //   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    
+  //   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+  //   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.]
+
+  // ISC License
+  // [Copyright <YEAR> <OWNER>
+
+  // Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+  // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.]
+
+  // N/A
+  
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  1. [ Description. ](#description)
+  2. [ Installation Instructions ](#installationInstructions)
+  3. [ Usage ](#usage)
+  4. [ Contributions ](#contributions)
+  5. [ Test Instructions ](#testInstructions)
+  6. [ License ](#license)
 
-`;
+  <a name="description"></a>
+  ## 1. Description
+  ${data.description}
+
+  <a name="installationInstructions"></a>
+  ## 2. Installation Instructions
+  ${data.installationInstructions}
+
+  <a name="usage"></a>
+  ## 3. Usage
+  ${data.usage}
+
+  <a name="contributions"></a>
+  ## 4. Contributions
+  ${data.contributions}
+
+  <a name="testInstructions"></a>
+  ## 5. Test Instructions
+  ${data.testInstructions}
+
+  <a name="license"></a>
+  ## 6. License
+  ${data.license}
+  `
 }
 
 module.exports = generateMarkdown;
